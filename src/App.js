1d18b8e4607a640e5d90231964c4826256a37967
Andrey <raigon28@gmail.com>
2021-11-01 19:00:47 +0300

    dataBase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import axios from 'axios';
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+      axios.get('http://localhost:3000/db.json').then(res => setItems(res.data.pizzas));
+  }, [])
+
+  console.log(items)
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={items} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Categories from "./Categories";
 import SortPopup from "./SortPopup";
+import PizzaCard from "./PizzaCard";
 
-export { Header, Categories, SortPopup };
+export { Header, Categories, SortPopup, PizzaCard };
